7dae3963b75d7b56b3bf68792e59f0f48967df12
pastelstoic <pastelstoic@proton.me>
2024-01-23 15:49:12 -0500

    Added param management

--- api/api.ts
+++ api/api.ts
@@ -6,12 +6,23 @@ export default class Api {
   /**
    * Gets an endpoint from the DeviantArt api.
    */
-  // deno-lint-ignore no-explicit-any
-  public get = async (endpoint: string, params: any) => {
-    params = params.params ? params.params : params;
-    params.access_token = this.accessToken;
-    const url = apiURL + endpoint;
-    const result = await fetch(url, { body: params }).then(async (
+  public get = async (endpoint: string, params: { params: Record<string, string | number | boolean | undefined | string[]> }) => {
+    params.params.access_token = this.accessToken;
+    const url = new URL(apiURL + endpoint);
+
+    for (const pair of Object.entries(params.params)) {
+      if (pair[1] !== undefined) {
+        if (Array.isArray(pair[1])) {
+          for (const item of pair[1]) {
+            url.searchParams.set(encodeURIComponent(pair[0] + "[]"), item.toString());
+          }
+        } else {
+          url.searchParams.set(pair[0], pair[1].toString());
+        }
+      }
+    }
+
+    const result = await fetch(url).then(async (
       r,
     ) => await r.json());
     return result;
@@ -20,10 +31,14 @@ export default class Api {
   /**
    * Used internally to get the access token in [[login]]
    */
-  // deno-lint-ignore no-explicit-any
-  public static getNoLogin = async (endpoint: string, params: any) => {
-    const url = apiURL + endpoint;
-    const result = await fetch(url, { body: params }).then(async (
+  public static getNoLogin = async (endpoint: string, params: Record<string, string | number | boolean | undefined>) => {
+    const url = new URL(apiURL + endpoint);
+    for (const pair of Object.entries(params)) {
+      if (pair[1] !== undefined) {
+        url.searchParams.set(pair[0], pair[1].toString());
+      }
+    }
+    const result = await fetch(url).then(async (
       r,
     ) => await r.json());
     return result;
