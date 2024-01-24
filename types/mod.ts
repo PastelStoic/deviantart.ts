596c2217b6a2c747fb1c9f40090587e94944e300
pastelstoic <pastelstoic@proton.me>
2024-01-24 11:45:43 -0500

    Added error type

--- types/mod.ts
+++ types/mod.ts
@@ -6,3 +6,10 @@ export * from "./GalleryTypes.ts";
 export * from "./CollectionTypes.ts";
 export * from "./CommentTypes.ts";
 export * from "./StashTypes.ts";
+
+export type DeviantArtError = {
+    error: "invalid_request" | "unauthorized" | "unverified_account" | "server_error" | "version_error";
+    error_description: string;
+    error_code: number;
+    status: string;
+}
